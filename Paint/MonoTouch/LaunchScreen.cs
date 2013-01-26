96a015e6413b012ebe8b01b438d348219476334d
Randolph Burt <randolph.burt@gmail.com>
2013-01-26 21:09:59 +0000

    Changed launch images to refer to "paint replay" instead of "iPaint"

--- Paint/MonoTouch/LaunchScreen.cs
+++ Paint/MonoTouch/LaunchScreen.cs
@@ -1,18 +1,51 @@
-
-using System;
-using System.Drawing;
-
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
-
+/// <summary>
+/// LanguageStrings.cs
+/// Randolph Burt - January 2013
+/// </summary>
 namespace Paint
 {
+	using System;
+	
+	using MonoTouch.Foundation;
+	using MonoTouch.UIKit;
+
+	/// <summary>
+	/// The application launch screen.
+	/// </summary>
 	public partial class LaunchScreen : UIViewController
 	{
+		/// <summary>
+		/// Has the exit process already started / been completed.  
+		/// Ensures we don't raise the LAunchScreenComplete event twice (e.g. use touching the screen and the timer completing)
+		/// </summary>
+		private bool exitInitiated = false;
+
+		/// <summary>
+		/// The timer for making the background images appear
+		/// </summary>
+		private NSTimer timer = null;
+
+		/// <summary>
+		/// Gets or sets a value indicating whether the user is allowed to exit early 
+		/// (ie. by touching the screen)
+		/// </summary>
+		public bool AllowUserExit
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Paint.LaunchScreen"/> class.
+		/// </summary>
 		public LaunchScreen() : base ("LaunchScreen", null)
 		{
 		}
-		
+
+		/// <Docs>Called when the system is running low on memory.</Docs>
+		/// <summary>
+		/// Dids the receive memory warning.
+		/// </summary>
 		public override void DidReceiveMemoryWarning()
 		{
 			// Releases the view if it doesn't have a superview.
@@ -21,20 +54,44 @@ namespace Paint
 			// Release any cached data, images, etc that aren't in use.
 		}
 
+		/// <summary>
+		/// Occurs when launch screen complete.
+		/// </summary>
 		public event EventHandler LaunchScreenComplete;
-		
+
+		/// <Docs>Called after the controller’s view is loaded into memory.</Docs>
+		/// <summary>
+		/// This view has loaded
+		/// </summary>
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
 
-			// TODO - Consider @2x - also better path than going through DefaultImages...
 			this.titleImageView.Image = UIImage.FromBundle("Content/launchTitle.png");
 			this.authorImageView.Image = UIImage.FromBundle("Content/launchAuthor.png");
-			this.pictureImageView.Image = UIImage.FromBundle("Content/DefaultImages/StandardResolution/e2a9c783-d72a-4681-b083-47fd32874933/e2a9c783-d72a-4681-b083-47fd32874933.JPG");
-
-			// Perform any additional setup after loading the view, typically from a nib.
+			this.pictureImageView.Image = UIImage.FromBundle("Content/launchImage.jpg");
 		}
 
+		/// <Docs>Set containing the touches.</Docs>
+		/// <summary>
+		/// Sent when one or more fingers touches the screen.
+		/// </summary>
+		/// <param name="touches">Touches.</param>
+		/// <param name="evt">Evt.</param>
+		public override void TouchesBegan(NSSet touches, UIEvent evt)
+		{
+			base.TouchesBegan(touches, evt);
+
+			if (this.AllowUserExit)
+			{
+				this.OnLaunchScreenComplete(EventArgs.Empty);
+			}
+		}
+
+		/// <summary>
+		/// This view is now on screen
+		/// </summary>
+		/// <param name="animated">If set to <c>true</c> animated.</param>
 		public override void ViewDidAppear(bool animated)
 		{
 			base.ViewDidAppear(animated);
@@ -51,19 +108,29 @@ namespace Paint
 				});
 			});
 
-			// TODO - make timer a local property so we can properly dispose of it
-			var timer = NSTimer.CreateScheduledTimer(6f, () => {
+			this.timer = NSTimer.CreateScheduledTimer(6f, () => {
 				this.InvokeOnMainThread(() => {
+					this.timer.Dispose();
+					this.timer = null;
 					this.OnLaunchScreenComplete(EventArgs.Empty);
 				});
 			});
 		}
 
+		/// <summary>
+		/// Raises the launch screen complete event.
+		/// </summary>
+		/// <param name="e">E.</param>
 		protected virtual void OnLaunchScreenComplete(EventArgs e)
 		{
-			if (this.LaunchScreenComplete != null)
+			if (!this.exitInitiated)
 			{
-				this.LaunchScreenComplete(this, EventArgs.Empty);
+				this.exitInitiated = true;
+
+				if (this.LaunchScreenComplete != null)
+				{
+					this.LaunchScreenComplete(this, EventArgs.Empty);
+				}
 			}
 		}
 	}
